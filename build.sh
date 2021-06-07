0ed266a46532d5933f1302116cbfbcbf58c67ef6
inokappa <inokara@gmail.com>
2021-06-08 06:54:41 +0900

    ビルドオプションに -trimpath を追加

--- build.sh
+++ build.sh
@@ -8,9 +8,14 @@ fi
 _BIN_NAME=rdstool
 
 rm ./pkg/*
-GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_linux_amd64
+GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_linux_amd64
 upx ./pkg/${_BIN_NAME}_linux_amd64
-GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_windows_amd64.exe
+GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_windows_amd64.exe
 upx ./pkg/${_BIN_NAME}_windows_amd64.exe
-GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_darwin_amd64
+GOOS=darwin GOARCH=amd64 go build -trimpath -ldflags="-w -s" -o ./pkg/${_BIN_NAME}_darwin_amd64
 upx ./pkg/${_BIN_NAME}_darwin_amd64
+
+# reference
+# https://golang.org/cmd/link/
+# https://golang.org/cmd/go/#hdr-Compile_packages_and_dependencies
+# https://devlights.hatenablog.com/entry/2020/10/21/192615
